c38b4229bd95b8f30ed4be8fbc31777c707ca620
Yaroslav <gso218695@gmail.com>
2021-08-16 21:54:03 +0300

    main layout

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,27 @@
 <template>
   <div id="app">
-    <router-view/>
+    <component :is="layout">
+      <router-view/>
+    </component>
   </div>
 </template>
 
+<script>
+import Main from '@/views/Main'
+export default {
+  computed: {
+    layout () {
+      console.log(this.$route.meta)
+      return 'main'
+    }
+  },
+  components: {
+    Main
+  }
+}
+</script>
+
 <style lang="scss">
+@import '~materialize-css/dist/css/materialize.min.css';
+@import 'assets/index.css';
 </style>
